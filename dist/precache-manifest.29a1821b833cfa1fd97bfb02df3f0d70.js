80b7a6d6e09db5e8ef24e9861e6f2627aea5bf0b
Latip <magomedovlatip0@gmail.com>
2021-05-19 18:33:48 +0300

    1.0

---
+++ dist/precache-manifest.29a1821b833cfa1fd97bfb02df3f0d70.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a6cf53ba2c8cfd8f70e",
+    "url": "/css/app.dd2ddff1.css"
+  },
+  {
+    "revision": "b2f63da940d9dadc06ad",
+    "url": "/css/chunk-vendors.0f92c67e.css"
+  },
+  {
+    "revision": "a44cb14ed72964a9849a1cf40cd2f4a6",
+    "url": "/img/1.a44cb14e.jpg"
+  },
+  {
+    "revision": "2953eef5cbd8776384f39a33029ae8c4",
+    "url": "/img/2.2953eef5.jpg"
+  },
+  {
+    "revision": "3b6cd5da231216a73a0b9f83f468d6e8",
+    "url": "/img/3.3b6cd5da.jpg"
+  },
+  {
+    "revision": "5fa553a5579410485626095a9434c35f",
+    "url": "/img/arrow.5fa553a5.svg"
+  },
+  {
+    "revision": "3d218e664ea64a0c583da3e4d1397c89",
+    "url": "/img/search.3d218e66.svg"
+  },
+  {
+    "revision": "663344e444686547770a65b60190e6cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a6cf53ba2c8cfd8f70e",
+    "url": "/js/app.7e890afa.js"
+  },
+  {
+    "revision": "b2f63da940d9dadc06ad",
+    "url": "/js/chunk-vendors.c76f0647.js"
+  },
+  {
+    "revision": "458234fc6be47cef7c9146de12cc7556",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
